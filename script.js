5b2076390012139a62bfa051c0850c033ff4bc3c
brunobiletsky <brunobiletsky@gmail.com>
2024-05-16 09:15:26 -0300

    criando slideshow e adicionando imagens

---
+++ script.js
@@ -0,0 +1,21 @@
+//Criando slideshow automatico
+
+//declarando as variaveis
+
+let imagens = ['./img/capitao-america.webp', './img/capitao-e-pantera.jpg', './img/viuva.webp'];
+let index = 0;
+let time = 3000; //em (ms)
+
+function slideshow(){
+    document.getElementById('banner').src=imagens[index];
+    index++;
+
+    if(index == imagens.length){
+        index = 0;
+
+    }
+    setTimeout('slideshow()', time)
+}
+slideshow();
+
+//menu hamburguer
